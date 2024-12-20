9bcb87694f0cf86146e620c86983247218e66c89
Elian Condor <econdor@krugerschool.edu.ec>
2024-12-20 08:17:01 -0800

    feat: install and configure Routes and PrimeReact

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primereact/resources/primereact.min.css";
+import { PrimeReactProvider } from "primereact/api";
+//import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <PrimeReactProvider>
+  <BrowserRouter>
   <StrictMode>
     <App />
   </StrictMode>,
+  </BrowserRouter>
+  </PrimeReactProvider>
 )
