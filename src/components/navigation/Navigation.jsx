23bd805a413a3df35c96765eedb16f12ca7e2d43
Elian Condor <econdor@krugerschool.edu.ec>
2025-01-19 20:06:01 -0800

    ✨ feat(navigation): add dynamic user role navigation
    - integrate AuthContext to access user role
    - conditionally render 'Users' option for admin roles only

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,16 +1,22 @@
 import { Home, Building, PlusSquare, User } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
-
+import { useAuth } from '../../context/AuthContext';
 const navItems = [
   { href: '/dashboard', icon: Home, label: 'Home' },
   { href: '/flats', icon: Building, label: 'View Flats' },
   { href: '/create', icon: PlusSquare, label: 'Create Flat' },
   { href: '/profile', icon: User, label: 'Profile' },
+  {href: '/users', icon: User, label: 'Users'},
 ];
 
 export const Navigation = () => {
+  const {user} = useAuth();
   const navigate = useNavigate();
   const currentPath = window.location.pathname;
+  if(user.rol === 'USER'){
+    navItems.pop();
+  }
+ 
 
   const handleClick = (e, href) => {
     e.preventDefault(); // Evita la recarga de la página
